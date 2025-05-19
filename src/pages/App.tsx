38aa26917ea7e612aecc7205e7f6d01f42f75d3f
Porianesu <976384615@qq.com>
2025-05-19 19:36:38 +0800

    Update: add CardFloor

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,16 +5,19 @@ import styles from './App.module.css'
 
 function App() {
   return (
-    <div className={styles.cardContainer}>
-      {Array.from(Array(5)).map((_, index) => (
-        <Card
-          style={{
-            transform: `translateY(${Math.abs(index - 2) * -30}px) translateZ(${(2 - Math.abs(index - 2)) * 70}px)`,
-            zIndex: 5 - Math.abs(index - 2), // 中间的卡片在最前面
-          }}
-          key={index}
-        ></Card>
-      ))}
+    <div className={styles.cardWrapper}>
+      <div className={styles.cardContainer}>
+        {Array.from(Array(5)).map((_, index) => (
+          <Card
+            style={{
+              transform: `translateY(${Math.abs(index - 2) * -30}px) translateZ(${(2 - Math.abs(index - 2)) * 100}px)`,
+              zIndex: 5 - Math.abs(index - 2), // 中间的卡片在最前面
+            }}
+            key={index}
+          ></Card>
+        ))}
+      </div>
+      <div className={styles.cardFloor}></div>
     </div>
   )
 }
