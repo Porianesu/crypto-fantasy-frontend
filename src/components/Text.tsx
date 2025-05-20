bd61c6adf7b9a9eb5c1164303055d1571e8c5fd3
Porianesu <976384615@qq.com>
2025-05-20 17:07:39 +0800

    Update: fix 3d animation in Text.tsx

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,7 +4,7 @@ import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect.tsx'
 import { gsap } from 'gsap'
 import classNames from 'classnames'
 
-const Text: React.FC = () => {
+const Text: React.FC<{ text: string }> = ({ text }) => {
   const containerRef = useRef<HTMLDivElement>(null)
   const words = useRef<Array<HTMLSpanElement>>([])
   const addWord = (ref: HTMLSpanElement) => {
@@ -17,31 +17,32 @@ const Text: React.FC = () => {
         transform: 'translate3d(-20px, 80px, 0px) rotateX(-60deg) rotateY(-20deg) rotateZ(-10deg)',
       })
       const tl = gsap.timeline({})
-      words.current.forEach((word, index) => {
-        tl.to(
-          word,
-          {
-            opacity: 1,
-            transform: 'translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)',
-            duration: 0.5,
-          },
-          index * 0.1,
-        )
+      tl.to(words.current, {
+        opacity: 1,
+        transform: 'translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)',
+        duration: 0.5,
+        stagger: 0.02,
       })
     }, containerRef)
     return () => ctx.revert()
   }, [])
   return (
-    <div ref={containerRef} className={classNames('max-w-[1200px] mx-auto px-4 py-8 text-white')}>
-      {'DSP Boost mechanism is aimed at motivating behaviors that contribute to community expansion, such as inviting new members to join DEFEDAO and frequently using DEFED products. These behaviors will help upgrade your Soul Bound Token and grant you more privileges within the DEFED ecosystem.'
-        .split('')
-        .map((word, index) => {
-          return (
-            <span key={`${word}-${index}`} ref={addWord}>
-              {word}
-            </span>
-          )
-        })}
+    <div
+      ref={containerRef}
+      className={classNames(
+        'max-w-[1200px] mx-auto px-4 py-8 text-white',
+        'perspective-midrange',
+        'text-2xl',
+        'whitespace-pre-wrap',
+      )}
+    >
+      {text.split('').map((word, index) => {
+        return (
+          <span className={'inline-block'} key={`${word}-${index}`} ref={addWord}>
+            {word}
+          </span>
+        )
+      })}
     </div>
   )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -41,7 +41,11 @@ function App() {
         </div>
         <div className={styles.cardFloor}></div>
       </div>
-      <Text></Text>
+      <Text
+        text={
+          'I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.Five score years ago a great American in whose symbolic shadow we stand today signed the\n Emancipation Proclamation. This momentous decree is a great beacon light of hope to millions of Negro slaves who had been seared in the flames of withering injustice. It came as a joyous daybreak to end the long night of their captivity. But 100 years later the Negro still is not free. One hundred years later the life of the Negro is still badly crippled by the manacles of segregation and the chains of discrimination. One hundred years later the Negro lives on a lonely island of poverty in the midst of a vast ocean of material prosperity. One hundred years later the Negro is still languished in the corners of American society and finds himself in exile in his own land. So we’ve come here today to dramatize a shameful condition.'
+        }
+      ></Text>
     </>
   )
 }
