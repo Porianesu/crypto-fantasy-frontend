3c73a6b16cfdd1b5bc6f209322875969611e459c
Porianesu <976384615@qq.com>
2025-05-28 17:14:10 +0800

    Update: add lazy load for pages

--- src/main.tsx
+++ src/main.tsx
@@ -16,10 +16,10 @@ import { TransitionProvider } from '@/context/TransitionContext.tsx'
 import React, { type PropsWithChildren, Suspense } from 'react'
 import PageContainer from '@/components/PageContainer.tsx'
 import TransitionComponent from '@/components/Transition.tsx'
-import LoadingPage from '@/pages/EntrancePage/EntrancePage.tsx'
-import HomePage from '@/pages/HomePage/HomePage.tsx'
-import CardPage from '@/pages/CardPage.tsx'
-import IntroductionPage from '@/pages/IntroductionPage/IntroductionPage.tsx'
+import EntrancePage from '@/pages/EntrancePage/EntrancePage.tsx'
+const HomePage = React.lazy(() => import('@/pages/HomePage/HomePage.tsx'))
+const CardPage = React.lazy(() => import('@/pages/CardPage.tsx'))
+const IntroductionPage = React.lazy(() => import('@/pages/IntroductionPage/IntroductionPage.tsx'))
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -46,7 +46,7 @@ export const router = createBrowserRouter([
         path: ENTRANCE_PATH,
         element: (
           <TransitionComponent>
-            <LoadingPage></LoadingPage>
+            <EntrancePage></EntrancePage>
           </TransitionComponent>
         ),
       },
