c9892353dec3bd74427c0e2b8d296267ec340c17
Porianesu <976384615@qq.com>
2025-06-03 10:06:15 +0800

    Update: add ui for IntroductionPage.tsx

--- src/pages/IntroductionPage/IntroductionPage.tsx
+++ src/pages/IntroductionPage/IntroductionPage.tsx
@@ -23,21 +23,20 @@ const IntroductionPage: React.FC = () => {
     <div className={styles.pageContainer}>
       <div className={styles.textContainer}>
         <Text className={classNames(styles.introduction)}>
-          {
-            '    In the beginning was the Word. The Word was close beside God, and the Word was God. 2 In the beginning, he was close beside God.'
-          }
-          <div className={'my-4'} />
-          {
-            '    All things came into existence through him; not one thing that exists came into existence without him. 4 Life was in him, and this life was the light of the human race. 5 The light shines in the darkness, and the darkness did not overcome it.'
-          }
-          <div className={'my-4'} />
-          {
-            '    There was a man called John, who was sent from God. 7 He came as evidence, to give evidence about the light, so that everyone might believe through him. 8 He was not himself the light, but he came to give evidence about the light.'
-          }
-          <div className={'my-4'}></div>
-          {
-            '    The true light, which gives light to every human being, was coming into the world. 10 He was in the world, and the world was made through him, and the world did not know him. 11 He came to what was his own, and his own people did not accept him. 12 But to anyone who did accept him, he gave the right to become God’s children; yes, to anyone who believed in his name. 13 They were not born from blood, or from fleshly desire, or from the intention of a man, but from God.'
-          }
+          {'Since the dawn of Bitcoin, the civilization of blockchain has rapidly \n' +
+            "expanded. Satoshi's dream evolvedinto the Interchain Realms-a world \n" +
+            'of countless projects coexisting. Each public chain is like acontinent, birthing \n' +
+            'unique ecosystems, economic systems, and belief cultures:'}
+          <div className={'my-2'} />
+          {'・Solana, where speed and frenzy reign,\n' +
+            '・Ethereum, master of structure and order,\n' +
+            '・Base, the cutting-edge frontier of tech...\n' +
+            'For ages, two opposing primal forces have ruled these realms:\n' +
+            'The Bullof Fire s.The Bear of Tide\n' +
+            'But now...Chaos stirs once more.\n' +
+            'Legends speak of a Cryptowalker-one,who wields the "Eye of Asset Control"-destined to lead a"Strategy War" that will alter the fate of the chains.\n' +
+            'And you... are the Chosen One.\n' +
+            'Follow in their footsteps. Awaken the slumbering Chainspirits. Battle through trading labyrinths. Risevictorious in the Grand Tournament. And ultimately...tame the Bull and Bear, breaking through the Gates of Destiny.'}
         </Text>
       </div>
       <button className={styles.skipButton} onClick={handleStartButtonClick}>
