baf6e512dcd912ec7cbf43178c7f46f805f85738
Porianesu <976384615@qq.com>
2025-06-03 10:08:10 +0800

    Update: add preload for IntroductionPage.tsx

--- src/stores/preloadManifest.ts
+++ src/stores/preloadManifest.ts
@@ -1,22 +1,16 @@
 export default [
   {
-    id: 'invite',
-    src: '/src/assets/sound/invite.ogg',
-  },
-  {
-    id: 'notification',
-    src: '/src/assets/sound/notification.ogg',
-  },
-  {
-    id: 'reactLogo',
-    src: '/src/assets/react.svg',
-  },
-  {
     id: 'cardsData',
     src: '/src/assets/json/cards_data.json',
   },
   {
     id: 'remoteVideo',
     src: 'https://cdn.coverr.co/videos/user-ai-generation-YKAem45Y8p/720p.mp4',
+  },
+  {
+    src: '/src/assets/images/introduction_page/background.png',
+  },
+  {
+    src: '/src/assets/images/introduction_page/start_button_background.png',
   },
 ]
