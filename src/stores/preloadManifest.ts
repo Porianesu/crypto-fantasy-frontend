9ae5a592e0f2bc748e9b2be60dce6e614e09661c
Porianesu <976384615@qq.com>
2025-06-04 10:58:04 +0800

    Update: update preloadManifest.ts

--- src/stores/preloadManifest.ts
+++ src/stores/preloadManifest.ts
@@ -43,4 +43,85 @@ export default [
   {
     src: '/src/assets/images/cards/card_border_3.png',
   },
+  {
+    src: '/src/assets/images/home_page/asset_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/asset_icon_1.png',
+  },
+  {
+    src: '/src/assets/images/home_page/asset_icon_2.png',
+  },
+  {
+    src: '/src/assets/images/home_page/asset_plus_button.png',
+  },
+  {
+    src: '/src/assets/images/home_page/avatar_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/card_formation_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/card_formation_plus_icon.png',
+  },
+  {
+    src: '/src/assets/images/home_page/draw_cards_modal_close_button.png',
+  },
+  {
+    src: '/src/assets/images/home_page/draw_cards_modal_title.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_achs.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_bag.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_battle.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_fusion.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_reward.png',
+  },
+  {
+    src: '/src/assets/images/home_page/footer_button_shop.png',
+  },
+  {
+    src: '/src/assets/images/home_page/header_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/leaderboard_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/leaderboard_current_user_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/leaderboard_item_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/notification_icon.png',
+  },
+  {
+    src: '/src/assets/images/home_page/open_package.png',
+  },
+  {
+    src: '/src/assets/images/home_page/open_package_button_background.png',
+  },
+  {
+    src: '/src/assets/images/home_page/rank_icon_1.png',
+  },
+  {
+    src: '/src/assets/images/home_page/rank_icon_2.png',
+  },
+  {
+    src: '/src/assets/images/home_page/rank_icon_3.png',
+  },
+  {
+    src: '/src/assets/images/home_page/setting_icon.png',
+  },
 ]
