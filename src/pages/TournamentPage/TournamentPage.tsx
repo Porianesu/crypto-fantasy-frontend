81fc85be8b7da0bf1980fe4954f74f974e778dd6
Porianesu <976384615@qq.com>
2025-06-12 17:25:36 +0800

    Update: update mock logic in TournamentPage.tsx

--- src/pages/TournamentPage/TournamentPage.tsx
+++ src/pages/TournamentPage/TournamentPage.tsx
@@ -23,44 +23,51 @@ enum PRIZE_POOL_STATUS {
   UPCOMING,
 }
 
+interface IPrizePool {
+  id: number
+  start_date: number
+  end_date: number
+  price: number
+  status: PRIZE_POOL_STATUS
+}
 const fetchPrizePools = async () => {
-  return new Promise((resolve) => {
+  return new Promise<Array<IPrizePool>>((resolve) => {
     const today = dayjs().startOf('day')
     const getRandomPrice = () =>
       new BigNumber(Math.random() * 490).plus(10).decimalPlaces(2).toNumber()
     resolve([
       {
         id: 0,
-        start_date: today.subtract(9, 'day'),
-        end_date: today.subtract(7, 'day').endOf('day'),
+        start_date: today.subtract(9, 'day').valueOf(),
+        end_date: today.subtract(7, 'day').endOf('day').valueOf(),
         price: getRandomPrice(),
         status: PRIZE_POOL_STATUS.END,
       },
       {
         id: 1,
-        start_date: today.subtract(6, 'day'),
-        end_date: today.subtract(4, 'day').endOf('day'),
+        start_date: today.subtract(6, 'day').valueOf(),
+        end_date: today.subtract(4, 'day').endOf('day').valueOf(),
         price: getRandomPrice(),
         status: PRIZE_POOL_STATUS.END,
       },
       {
         id: 2,
-        start_date: today.subtract(3, 'day'),
-        end_date: today.subtract(1, 'day').endOf('day'),
+        start_date: today.subtract(3, 'day').valueOf(),
+        end_date: today.subtract(1, 'day').endOf('day').valueOf(),
         price: getRandomPrice(),
         status: PRIZE_POOL_STATUS.END,
       },
       {
         id: 3,
-        start_date: today,
-        end_date: today.add(2, 'day').endOf('day'),
+        start_date: today.valueOf(),
+        end_date: today.add(2, 'day').endOf('day').valueOf(),
         price: getRandomPrice(),
         status: PRIZE_POOL_STATUS.PROCESSING,
       },
       {
         id: 4,
-        start_date: today.add(3, 'day'),
-        end_date: today.add(5, 'day').endOf('day'),
+        start_date: today.add(3, 'day').valueOf(),
+        end_date: today.add(5, 'day').endOf('day').valueOf(),
         price: getRandomPrice(),
         status: PRIZE_POOL_STATUS.UPCOMING,
       },
