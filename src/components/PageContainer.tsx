70c53d513877c0c086b80f57d9d0d64b437983fd
Porianesu <976384615@qq.com>
2025-06-18 10:39:34 +0800

    Update: add aspect container

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite'
-import React, { type PropsWithChildren } from 'react'
+import React, { type PropsWithChildren, useEffect } from 'react'
 import classNames from 'classnames'
 import { gsap } from 'gsap'
 import { SplitText } from 'gsap/SplitText'
@@ -9,19 +9,43 @@ gsap.registerPlugin(SplitText)
 gsap.registerPlugin(ScrollSmoother)
 
 const PageContainer: React.FC<PropsWithChildren> = ({ children }) => {
+  useEffect(() => {
+    function resizeContent() {
+      const container = document.getElementById('ratio-container')
+      if (!container) return
+      const ww = window.innerWidth
+      const wh = window.innerHeight
+      if (ww / wh > 16 / 9) {
+        // 屏幕更宽
+        container.style.width = `${(wh * 16) / 9}px`
+        container.style.height = `${wh}px`
+      } else {
+        // 屏幕更高
+        container.style.width = `${ww}px`
+        container.style.height = `${(ww * 9) / 16}px`
+      }
+    }
+    resizeContent()
+    window.addEventListener('resize', resizeContent)
+    return () => {
+      window.removeEventListener('resize', resizeContent)
+    }
+  }, [])
   return (
     <div
       className={classNames(
         'h-screen',
         'w-screen',
         'flex',
-        'flex-col',
-        'items-stretch',
+        'items-center',
+        'justify-center',
         'bg-black',
         'overflow-hidden',
       )}
     >
-      {children}
+      <div id={'ratio-container'} className={classNames('flex', 'flex-col', 'items-stretch')}>
+        {children}
+      </div>
     </div>
   )
 }
