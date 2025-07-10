04e683801a1658f0ccc1cdd3c7a2dd29950c1b96
Porianesu <976384615@qq.com>
2025-07-10 19:08:27 +0800

    Update: enhance Craft component layout with checklist container and Tailwind CSS styling

--- src/pages/FusionPage/Craft.tsx
+++ src/pages/FusionPage/Craft.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 import styles from './Craft.module.css'
 
 const Craft: React.FC = () => {
-  return <div className={styles.bodyContainer}></div>
+  return (
+    <div className={styles.bodyContainer}>
+      <div className={styles.checklistContainer}></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
 }
 export default observer(Craft)
