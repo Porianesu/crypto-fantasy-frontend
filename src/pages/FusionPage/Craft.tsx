0c7e6eb3dc7a160df751d0f1496fd613663bc7d1
Porianesu <976384615@qq.com>
2025-07-11 17:44:03 +0800

    Update: enhance Craft component with checklist and optional containers, and improve styling with Tailwind CSS

--- src/pages/FusionPage/Craft.tsx
+++ src/pages/FusionPage/Craft.tsx
@@ -1,14 +1,64 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Craft.module.css'
 import classNames from 'classnames'
+import type { ICardData } from '@/components/Card.tsx'
+import { useMobxStore } from '@/stores/StoreProvider.tsx'
 
 const Craft: React.FC = () => {
+  const {
+    appStore: { userInfo },
+  } = useMobxStore()
+  const [craftTargetCard, setCraftTargetCard] = useState<ICardData>()
+  const [additiveCards, setAdditiveCards] = useState<Array<ICardData>>([])
+
   return (
     <div className={styles.bodyContainer}>
-      <div className={styles.checklistContainer}></div>
+      <div className={styles.checklistContainer}>
+        <div className={styles.title}>Checklist</div>
+        <div className={styles.checklistTarget}>
+          <div>Target</div>
+          {craftTargetCard ? <div className={styles.targetCard}></div> : <div>???</div>}
+        </div>
+        <div className={styles.requiredContainer}>
+          <div>Required</div>
+          <div>
+            {craftTargetCard ? (
+              <div className={styles.requiredCardContainer}></div>
+            ) : (
+              <div>???</div>
+            )}
+            {craftTargetCard ? (
+              true ? (
+                <div className={styles.clearThrough}></div>
+              ) : (
+                <div className={styles.notClearThrough}></div>
+              )
+            ) : null}
+          </div>
+          <div>
+            <div>
+              {craftTargetCard ? 123 : '???'}
+              <div className={styles.requiredAssetIcon}></div>
+            </div>
+            {craftTargetCard ? (
+              true ? (
+                <div className={styles.clearThrough}></div>
+              ) : (
+                <div className={styles.notClearThrough}></div>
+              )
+            ) : null}
+          </div>
+        </div>
+        <div className={styles.optionalContainer}>
+          <div>Optional</div>
+          <div className={styles.optionalCount}>{`(${additiveCards.length}/4)`}</div>
+        </div>
+      </div>
       <div className={styles.middleContainer}>
-        <div className={styles.magicContainer}></div>
+        <div className={styles.magicContainer}>
+          <div className={styles.magicBackground}></div>
+        </div>
         <button className={classNames(styles.craftButton, 'button text-shadow')}>
           Craft 256<div className={styles.assetIcon}></div>
         </button>
